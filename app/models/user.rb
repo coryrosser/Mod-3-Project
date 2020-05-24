88344330b03d5b843dfe687729f8bf4897e8e0be
github username <github email address>
2020-05-24 19:20:31 -0400

    validations on user, updates to session controller, update to create user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
     has_one :profile 
     has_many :line_items
     has_many :trades
+
+    validates :first_name, :last_name, :email, presence: true, uniqueness: true 
+    validates :location, presence: true  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :items
   resources :profiles
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   
