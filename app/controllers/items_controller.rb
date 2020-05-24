2472d9220ade25144e65940aef88eb7a07c4bf54
coryrosser <coryrosser@gmail.com>
2020-05-24 12:19:15 -0400

    user and item controller. item trade rating calculation done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,46 @@
 class ItemsController < ApplicationController
+    before_action :find_item, only: [:show, :update, :destroy]
+    def index 
+        items = Item.all 
+
+        render :json => items
+    end
+    def show 
+        render json: item
+    end
+
+    def create 
+        @item = Item.new(item_params)
+        @item.trade_rating = @item.calculate_trade_rating
+        #check Item model for trade rating method and explanation
+        if @item.save 
+            render json: @item, status: :created
+
+        else 
+            render json: @item.errors, status: :error
+        end
+
+    end
+    def update
+        if @item.update(item_params)
+         render json: @item
+        else
+         render json: @item.errors, status: :error
+        end
+       end
+
+    def destroy
+        item.destroy 
+    end
+
+    private 
+
+    def find_item 
+        item = Item.find_by(id: params[:id])
+    end
+
+    def item_params 
+        params.require(:item).permit(:brand, :model, :description, :condition, :retail_value, :finish, :user_id)
+    end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ApplicationRecord
     has_one :profile 
     has_many :line_items
     has_many :trades
-
-
 end
